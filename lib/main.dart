ee7255f25ac7c6b1d8814ed731eaf2e3c56cf1d9
sreedeviluminar <sreedevi@luminartechnolab.com>
2023-03-07 15:11:13 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -24,13 +24,13 @@ class MapSampleState extends State<MapSample> {
   Completer<GoogleMapController> _controller = Completer();
 
   static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
+    target: LatLng(10.0159, 76.3419),
     zoom: 14.4746,
   );
 
   static final CameraPosition _kLake = CameraPosition(
       bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
+      target: LatLng(10.0038, 76.3427),
       tilt: 59.440717697143555,
       zoom: 19.151926040649414);
 
